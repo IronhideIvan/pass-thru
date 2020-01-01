3883e0647f92587dd4ab058ae6dc6351e139e1f7
Arthur Lisek-Koper <arthurlisekkoper@gmail.com>
2019-12-31 18:50:25 -0600

    Add buttons to send random input signals
    Additionally, cleaned up the debug UI significantly now that I better
    understand how things are laid out.

--- android/app/src/main/java/com/passthru/android/ui/notifications/DebuggerFragment.kt
+++ android/app/src/main/java/com/passthru/android/ui/notifications/DebuggerFragment.kt
@@ -1,54 +1,89 @@
 package com.passthru.android.ui.notifications
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.passthru.android.R
 import com.passthru.android.util.Globals
+import com.passthru.android.util.InputDispatcher
 import com.passthru.android.util.InputReport
-import kotlinx.android.synthetic.main.fragment_debugger.*
+import com.passthru.android.util.UdpHelper
+import java.security.Key
+import kotlin.random.Random
 
 class DebuggerFragment : Fragment() {
 
-    private lateinit var debuggerViewModel: DebuggerViewModel
-    private lateinit var tvXAxis: TextView
-    private lateinit var tvYAxis: TextView
-    private lateinit var tvZAxis: TextView
-    private lateinit var tvRxAxis: TextView
-    private lateinit var tvRyAxis: TextView
-    private lateinit var tvRzAxis: TextView
-    private lateinit var tvThrottle: TextView
-    private lateinit var tvBrake: TextView
+    private lateinit var vm: DebuggerViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        debuggerViewModel =
+        vm =
             ViewModelProviders.of(this).get(DebuggerViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_debugger, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
-        debuggerViewModel.text.observe(this, Observer {
+        val tvXAxis: TextView = root.findViewById((R.id.lblAxisXVal))
+        val tvYAxis: TextView = root.findViewById((R.id.lblAxisYVal))
+        val tvZAxis: TextView = root.findViewById((R.id.lblAxisZVal))
+        val tvRxAxis: TextView = root.findViewById((R.id.lblAxisRxVal))
+        val tvRyAxis: TextView = root.findViewById((R.id.lblAxisRyVal))
+        val tvRzAxis: TextView = root.findViewById((R.id.lblAxisRzVal))
+        val tvThrottle: TextView = root.findViewById((R.id.lblThrottleVal))
+        val tvBrake: TextView = root.findViewById((R.id.lblBrakeVal))
+        val tvButtons: TextView = root.findViewById((R.id.lblButtonsVal))
+
+        vm.headerText.observe(this, Observer {
             textView.text = it
         })
 
-        tvXAxis = root.findViewById((R.id.lblAxisXVal))
-        tvYAxis = root.findViewById((R.id.lblAxisYVal))
-        tvZAxis = root.findViewById((R.id.lblAxisZVal))
-        tvRxAxis = root.findViewById((R.id.lblAxisRxVal))
-        tvRyAxis = root.findViewById((R.id.lblAxisRyVal))
-        tvRzAxis = root.findViewById((R.id.lblAxisRzVal))
-        tvThrottle = root.findViewById((R.id.lblThrottleVal))
-        tvBrake = root.findViewById((R.id.lblBrakeVal))
+        vm.xAxis.observe(this, Observer {
+            tvXAxis.text = it
+        })
+
+        vm.yAxis.observe(this, Observer {
+            tvYAxis.text = it
+        })
+
+        vm.zAxis.observe(this, Observer {
+            tvZAxis.text = it
+        })
+
+        vm.rXAxis.observe(this, Observer {
+            tvRxAxis.text = it
+        })
+
+        vm.rYAxis.observe(this, Observer {
+            tvRyAxis.text = it
+        })
+
+        vm.rZAxis.observe(this, Observer {
+            tvRzAxis.text = it
+        })
+
+        vm.throttle.observe(this, Observer {
+            tvThrottle.text = it
+        })
+
+        vm.brake.observe(this, Observer {
+            tvBrake.text = it
+        })
+
+        vm.buttons.observe(this, Observer {
+            tvButtons.text = it
+        })
 
         val cbDebugMode: CheckBox = root.findViewById((R.id.cbDebugMode))
+        cbDebugMode.isChecked = Globals.debugMode.get()
+        Globals.debuggerFragment.set(this)
+
         cbDebugMode.setOnCheckedChangeListener{ _, isChecked ->
             Globals.debugMode.set(isChecked)
             if(isChecked){
@@ -59,19 +94,49 @@ class DebuggerFragment : Fragment() {
             }
         }
 
-        Globals.debuggerFragment.set(this)
+        val btnSendSignal: Button = root.findViewById((R.id.btnSendMotionSignal))
+        btnSendSignal.setOnClickListener{
+            if(!UdpHelper.isConnected()){
+                Toast.makeText(context, "Not connected", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val motionEventCodes = intArrayOf(MotionEvent.AXIS_X, MotionEvent.AXIS_Y, MotionEvent.AXIS_Z, MotionEvent.AXIS_RX, MotionEvent.AXIS_RY, MotionEvent.AXIS_RZ, MotionEvent.AXIS_THROTTLE, MotionEvent.AXIS_BRAKE)
+            val motionEvent = MotionEvent.obtain(1, 1, motionEventCodes[Random.nextInt(0, motionEventCodes.size)], Random.nextFloat(), Random.nextFloat(), 0)
+            InputDispatcher.dispatchMotionEvent(motionEvent)
+        }
+
+        val btnSendButton: Button = root.findViewById((R.id.btnSendButtonSignal))
+        btnSendButton.setOnClickListener{
+            if(!UdpHelper.isConnected()){
+                Toast.makeText(context, "Not connected", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val actionCodes = intArrayOf(KeyEvent.ACTION_UP, KeyEvent.ACTION_DOWN)
+            val btnEventCodes = intArrayOf(KeyEvent.KEYCODE_DPAD_UP, KeyEvent.KEYCODE_DPAD_DOWN, KeyEvent.KEYCODE_DPAD_LEFT, KeyEvent.KEYCODE_DPAD_RIGHT, KeyEvent.KEYCODE_BUTTON_X,
+                KeyEvent.KEYCODE_BUTTON_Y, KeyEvent.KEYCODE_BUTTON_A, KeyEvent.KEYCODE_BUTTON_B)
+
+            val keyEvent: KeyEvent = KeyEvent(1, 1,
+                actionCodes[Random.nextInt(0, actionCodes.size)],
+                btnEventCodes[Random.nextInt(0, btnEventCodes.size)],
+            0, 0, 0, 0, 0, 0)
+
+            InputDispatcher.dispatchKeyEvent(keyEvent)
+        }
 
         return root
     }
 
     fun populateDebug(report: InputReport) {
-        tvXAxis.setText(report.axisReport.axis1.x.toString())
-        tvYAxis.setText(report.axisReport.axis1.y.toString())
-        tvZAxis.setText(report.axisReport.axis1.z.toString())
-        tvRxAxis.setText(report.axisReport.axis2.x.toString())
-        tvRyAxis.setText(report.axisReport.axis2.y.toString())
-        tvRzAxis.setText(report.axisReport.axis2.z.toString())
-        tvThrottle.setText(report.axisReport.throttle.toString())
-        tvBrake.setText(report.axisReport.brake.toString())
+        vm.xAxis.value = report.axisReport.axis1.x.toString()
+        vm.yAxis.value = report.axisReport.axis1.y.toString()
+        vm.zAxis.value = report.axisReport.axis1.z.toString()
+        vm.rXAxis.value = report.axisReport.axis2.x.toString()
+        vm.rYAxis.value = report.axisReport.axis2.y.toString()
+        vm.rZAxis.value = report.axisReport.axis2.z.toString()
+        vm.throttle.value = report.axisReport.throttle.toString()
+        vm.brake.value = report.axisReport.brake.toString()
+        vm.buttons.value = report.buttonReport.buttons.toString()
     }
 }
